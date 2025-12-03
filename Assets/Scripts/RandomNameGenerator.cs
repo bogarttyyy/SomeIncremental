e130b293f0058953eba88a55313ed138202ea991
Dennis Abad <dennis.abad6@gmail.com>
2025-12-03 16:20:44 +1100

    Changed names to be ethinically diverse

--- Assets/Scripts/RandomNameGenerator.cs
+++ Assets/Scripts/RandomNameGenerator.cs
@@ -3,26 +3,26 @@ using System;
 public static class RandomNameGenerator
 {
     static readonly string[] FirstNames = {
-        "Ava", "Liam", "Mia", "Noah", "Ella", "Ethan", "Zara", "Mason", "Isla", "Leo",
-        "Aria", "Jack", "Ruby", "Hudson", "Chloe", "Finn", "Sienna", "Owen", "Harper", "Caleb",
-        "Elodie", "Miles", "Nora", "Ivy", "Asher", "Luca", "Piper", "Theo", "Maren", "Silas",
-        "Veda", "Arlo", "Juniper", "Remy", "Callum", "Esme", "Wyatt", "Naomi", "Sawyer", "Freya",
-        "Jasper", "Lena", "Colt", "Adeline", "Rory", "Gideon", "Maeve", "Felix", "Dahlia", "Rowan",
-        "Clara", "Briar", "Emmett", "Poppy", "Hugo", "Tessa", "Atlas", "Mila", "Reese", "Cleo"
+        "Amina", "Mateo", "Priya", "Wei", "Leila", "Yusuf", "Hana", "Malik", "Sofia", "Diego",
+        "Mei", "Arjun", "Samira", "Luca", "Nia", "Omar", "Keiko", "Rafael", "Zahra", "Elias",
+        "Carmen", "Hiro", "Ayana", "Jonas", "Anika", "Tariq", "Ayesha", "Jin", "Fatima", "Kofi",
+        "Soren", "Amara", "Chen", "Imani", "Rina", "Naveen", "Marisol", "Dario", "Zaid", "Priyanka",
+        "Kenji", "Farah", "Emilio", "Anish", "Soraya", "Idris", "Yara", "Pavel", "Nikhil", "Sana",
+        "Dmitri", "Thandi", "Bashir", "Jae", "Ayoub", "Mireya", "Haruto", "Tenzin", "Sahar", "Kwame", "Charo"
     };
 
     static readonly string[] MiddleNames = {
         "Grace", "James", "Lee", "Rose", "Kai", "Skye", "Drew", "Quinn", "Jude", "Reid",
-        "Blair", "Brooke", "Wren", "Shay", "Cole", "Faye", "Hayes", "Rhys", "Lane", "Shea"
+        "Blair", "Brooke", "Wren", "Shay", "Cole", "Faye", "Hayes", "Rhys", "Lane", "Shea", "May"
     };
 
     static readonly string[] LastNames = {
-        "Hart", "Kelley", "Archer", "Sawyer", "Lennox", "Mercer", "Hale", "Rowan", "Parker", "Ellis",
-        "Sutton", "Monroe", "Bennett", "Dalton", "Greer", "Harlow", "Sinclair", "Tanner", "Wilder", "Langley",
-        "Callahan", "Drake", "Finch", "Granger", "Hastings", "Irwin", "Jarvis", "Keaton", "Larkin", "Maddox",
-        "Nolan", "Oakley", "Prescott", "Quincy", "Radford", "Sterling", "Thatcher", "Underwood", "Vaughn", "Winslow",
-        "York", "Carver", "Denton", "Easton", "Fletcher", "Gaines", "Hendricks", "Iverson", "Jensen", "Kerrigan",
-        "Lyle", "Montrose", "Newell", "Pryor", "Ridley", "Samuels", "Thorne", "Ulrich", "Vickers", "Whitman"
+        "Ahmed", "Martinez", "Nguyen", "Patel", "Haddad", "Okafor", "Chen", "Ivanov", "Kim", "Singh",
+        "Garcia", "Diallo", "Mori", "Khan", "Petrova", "Mensah", "Rahman", "Costa", "Kawamura", "Hussein",
+        "Alvarez", "Zhang", "Mbatha", "Lopez", "Yilmaz", "Das", "Silva", "Bello", "Torres", "Rossi",
+        "Baba", "Leblanc", "Abadines", "Tanaka", "Osei", "Volkov", "Saidi", "Popescu", "Chandra", "Mendez",
+        "DeSouza", "Bekele", "Hashimoto", "Romero", "Mwangi", "Ghosh", "Nakamura", "Conteh", "Fernandes", "Abiola",
+        "Karim", "Sato", "Noorani", "Kowalski", "Batista", "Rojas", "Onwu", "Esquivel", "Petrovic", "Delgado"
     };
 
     // Returns a full name: First Last, with a middle name about 10% of the time.
