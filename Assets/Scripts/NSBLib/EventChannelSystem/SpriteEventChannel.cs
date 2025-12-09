b06066af6502016ff224b93e9b091b9b22440145
Dennis Abad <dennis.abad6@gmail.com>
2025-12-09 20:54:01 +1100

    Added SpriteEventChannel and Listener

---
+++ Assets/Scripts/NSBLib/EventChannelSystem/SpriteEventChannel.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace NSBLib.EventChannelSystem
+{
+    [CreateAssetMenu(fileName = "SpriteEventChannel", menuName = "Events/SpriteEventChannel")]
+    public class SpriteEventChannel : EventChannel<Sprite> {}
+}
